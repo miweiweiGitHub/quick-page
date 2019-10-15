9cce918263e8c3877177966db41e9d8fa5ffddad
huolongguo <726466163@qq.com>
2019-10-15 16:38:36 +0800

    first add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
+    <div id="app-1">
+        <img src="./assets/logo.png">
+        <div>
+        <el-button @click="startHacking">Start</el-button>
+        </div>
     </div>
+    <route-view>
   </div>
 </template>
 
@@ -23,8 +26,44 @@ export default {
 </script>
 
 <style>
-#app {
+#app-1 {
   font-family: Helvetica, sans-serif;
   text-align: center;
+}
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
+ol,
+ul {
+    list-style: none;
+}
+a {
+    text-decoration: none;
+}
+
+html,
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-size: 14px;
+    background-color: aquamarine;
+    font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial,
+        sans-serif, 'Microsoft YaHei', '微软雅黑', STXihei, '华文细黑', serif;
+}
+
+::-webkit-scrollbar {
+    width: 4px;
+    height: 8px;
+}
+
+::-webkit-scrollbar-track {
+    background-color: rgba(255, 255, 255, 1);
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: rgba(135, 141, 153, 0.5);
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+
+const Vue = require('vue')
+const VueRouter = require('vue-router')
+
+const router = new VueRouter({
+    routes: constantRouterMap
+})
+
+export default router
